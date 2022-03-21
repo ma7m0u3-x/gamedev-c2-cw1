52d9da4e48f26f4510fba4daccdd231bcad9cba6
ma7m0u3-x <mahmoudx2005x@gmail.com>
2022-03-21 20:11:03 +0300

    اكواد البرمجة

---
+++ c2_cw1/c2_cw1/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace c2_cw1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string characterName = "spiderman";
+            string superPower1 = "sensor";
+            string superPower2 = "web launch";
+            double characterSpeed = 1.75;
+            int age = 24;
+
+
+            Console.WriteLine(" my name is {0}, my superPower1 is {1}, my suporPower2 is {2}, my age is {3}", characterName, superPower1, superPower2, characterSpeed, age);
+        }
+    }
+}
